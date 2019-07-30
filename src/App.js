9d2c84261cb3e0345190726f9fc3ea8aa59f7805
Christian Toscano <chrismarcadopago@gmail.com>
2019-07-30 15:56:13 -0300

    estilos para los botones y las cards

--- src/App.js
+++ src/App.js
@@ -34,29 +34,34 @@ class App extends Component {
     return (
       <div className="App">
 
-        <button 
-          onClick={() => this.nextProperty()} 
-          disabled={property.index === data.properties.length-1}
-        >Next</button>
-        <button 
-          onClick={() => this.prevProperty()} 
-          disabled={property.index === 0}
-        >Prev</button>
-
         <div className="page">
             <section>
                 <img src={logo} className="App-logo" alt="logo" />
-                <h1>Image slideshow React tutorial.</h1>
+                <h1>Image slideshow React.</h1>
             </section>
 
-          <div className="cards-slider">
-            <div className="cards-slider-wrapper">
+            <div className="buttons">
+              <a class="boton_personalizado"
+                onClick={() => this.nextProperty()} 
+                disabled={property.index === data.properties.length-1}
+              >Next</a>
+              <a class="boton_personalizado"
+                onClick={() => this.prevProperty()} 
+                disabled={property.index === 0}
+              >Prev
+              </a>
+            </div>
+
+          <div className={`cards-slider active-slide-${property.index}`}>
+            <div className="cards-slider-wrapper" style={{
+              'transform': `translateX(-${property.index*(100/properties.length)}%)`
+            }}>
               {
                 properties.map(property => <Card key={property._id} property={property} />)
               }
             </div>
-          </div>
-        </div>
+          </div>          
+        </div>       
       </div>
     );
   }
